d49fe83e7a5a27ddfa57f06266643dee8d8b6615
Jatin Sharma <jatinsharma13arril2002@gmail.com>
2023-08-22 11:50:59 +0530

    send

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,32 @@
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Hero from "@/components/Hero";
+import Project from "@/components/Project";
+import Skill from "@/components/Skill";
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="">Hello</main>;
+  return (
+    <main className="">
+      <section>
+        <Hero />
+      </section>
+
+      <section>
+        <About />
+      </section>
+
+      <section>
+        <Skill />
+      </section>
+
+      <section>
+        <Project />
+      </section>
+
+      <section>
+        <Contact />
+      </section>
+    </main>
+  );
 }
