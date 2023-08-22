4f0f9551227a588d011e922d80ec3e37fbc4ee73
Pallavi <dpallavi854@gmail.com>
2023-08-22 16:49:01 +0530

    navbar donr

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const About = () => {
   return <div className="flex justify-between h-screen pt-20">
     <img src="#" alt="img" />
-    <div className=" bg-pink-100 ps-3 pe-5 pb-3 pt-5 h-max rounded-lg text-left md:w-4/6">
+    <div className=" bg-pink-100 ps-3 pe-5 pb-3 pt-5 h-max text-left md:w-4/6">
       <h2 className="text-2xl font-bold">About Me</h2>
       <p className="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt deserunt repellat in, porro qui fugiat eligendi odio esse omnis beatae nobis voluptates neque dignissimos iure deleniti rem tenetur eius voluptate! Impedit et nam cupiditate laudantium expedita? Eaque, ut quisquam, facere explicabo quia in hic corporis repellat perferendis quibusdam error ipsum?</p>
       <div className="flex gap-4 mt-5">
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 import React from "react";
 import { BsArrowUpRight } from 'react-icons/bs'
+import { Abril_Fatface } from "next/font/google";
+import LetterAnimation from "./letterAnimation/LetterAnimation";
+import Image from "next/image";
 
+
+const headingFont = Abril_Fatface({subsets: ['latin'],weight: '400'})
 const Home = () => {
-  return <div className=" bg-white p-10">
+  return <div className=" bg-white p-10 flex">
+    <div>
+      <p>Hey! My name is</p>
+      <h1 className={headingFont.className}>PALLAVI DWIVEDI.</h1>
+      <LetterAnimation />
+      <div>
+        <Link href="/PALLAVI_DWIVEDI.pdf"
+        // alt="alt text"
+        target="_blank"
+        rel="noopener noreferrer">Resume</Link>
+        <Link href={"#"}>Contact Me</Link>
+      </div>
+    </div>
+    <Image src={"/images/character.svg"} width={200} height={200} alt="Pallavi Dwivedi" />
      </div>;
 };
 
