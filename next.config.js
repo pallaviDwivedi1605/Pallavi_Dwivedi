58c0ce4553348d8deb5572d644e59a4aa88b8c1b
Jatin Sharma <jatinsharma13arril2002@gmail.com>
2023-08-22 11:26:25 +0530

    branch added

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        // pathname: '/account123/**',
-      },
-    ],
-  },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
