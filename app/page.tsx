25aa386992336b8edfd84f35c37eba6a1bb97ca1
Jatin Sharma <jatinsharma13arril2002@gmail.com>
2023-08-22 12:01:32 +0530

    pd

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
 import Project from "@/components/Project";
 import Skill from "@/components/Skill";
-import Image from "next/image";
 
 export default function Home() {
   return (
