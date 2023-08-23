b546b325f4dc29cd60a3b499ed83d11c232485a7
Pallavi <dpallavi854@gmail.com>
2023-08-23 23:03:05 +0530

    project page on working

--- components/Project.tsx
+++ components/Project.tsx
@@ -3,14 +3,16 @@ import Card from "./utils/Card";
 
 const Project = () => {
   const projectData = [
-    {id:1, name:"project 1", link:"#"},
+    {id:1, name:"Dashboard", tech:"React | Highcharts", url:"/images/girl-php.jpg", desc:"nerhkfch xgjhkcrjnhkjeriucioirodfoijckcjwekjs,cnkv"},
+    {id:2, name:"Migrators", tech:"HTML | CSS | JS", url:"/images/girl-php.jpg", desc:"nerhkfch xgjhkcrjnhkjeriucioirodfoijckcjwekjs,cnkv"},
+    {id:3, name:"Simon", tech:"HTML | CSS | Jquery", url:"/images/girl-php.jpg", desc:"nerhkfch xgjhkcrjnhkjeriucioirodfoijckcjwekjs,cnkv"},
   ]
-  return <div className="bg-white pt-24 gap-4 flex flex-col items-start h-full w-full ps-6 pe-6 md:ps-16 md:pe-16">
+  return <div className=" pt-24 gap-4 flex flex-col items-start h-full w-full ps-6 pe-6 md:ps-16 md:pe-16">
     <h2 className="text-3xl font-bold text-primary tracking-wide">My Work</h2>
-    <p className=" text-lg">Here is some of my project work that showcase my skills. Click to visit the project.</p>
-    <div>
+    <p className=" text-lg text-left">Here is some of my project work that showcase my skills. Click to visit the project.</p>
+    <div className="flex flex-col gap-3 md:flex-row">
       {
-        projectData.map((obj)=><Card  />)
+        projectData.map((obj)=><Card key={obj.id} label={obj.name} tech={obj.tech} desc={obj.desc} image={obj.url}  />)
       }
     </div>
   </div>;
--- components/utils/Card.tsx
+++ components/utils/Card.tsx
@@ -1,8 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
+import Button from './Button'
 
-const Card = () => {
+const Card = ({image,label,tech,desc}:any) => {
   return (
-    <div>Card</div>
+    <div className=' group w-80 h-96 bg-white border relative rounded-2xl'>
+        <div className=' rounded-t-2xl h-2/3 w-full transition-all duration-300 ease-in-out bg- group-hover:opacity-10'><Image className=' rounded-t-2xl' src={image} alt='image' fill={true} /></div>
+        <div className=' rounded-b-2xl h-1/3 transition-all duration-300 ease-in-out group-hover:h-2/3 absolute bottom-0 w-full bg-white pt-2 flex flex-col items-center gap-2'>
+            <h3 className='text-xl font-semibold tracking-wide'>{label}</h3>
+            <p className=' tetx-lg font-medium'>{tech}</p>
+            <p className='hidden group-hover:block'>{desc}</p>
+            <Button name={"Visit Me"} href={"#"} />
+        </div>
+    </div>
   )
 }
 
