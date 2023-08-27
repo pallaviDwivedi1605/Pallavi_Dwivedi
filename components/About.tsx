8f1a86edbd9891e5fdb2bec50783a9ccedd711e1
Jatin Sharma <jatinsharma13arril2002@gmail.com>
2023-08-27 21:06:40 +0530

    added

--- components/About.tsx
+++ components/About.tsx
@@ -6,43 +6,36 @@ import FlipCard from "./utils/FlipCard";
 import { GiAchievement } from "react-icons/gi";
 import { FaGraduationCap } from "react-icons/fa";
 
+const cardData = [
+  {
+    id: 1,
+    skill: "React | Tailwind | Next",
+    url: "/images/girl-ui.jpg",
+    name: "Front-end",
+    desc: "I command HTML, CSS, and JavaScript proficiently, and my expertise extends to React, Tailwind CSS, and Next.js. This comprehensive skill set enables me to excel in frontend development, crafting dynamic and responsive user interfaces.",
+  },
+  {
+    id: 2,
+    skill: "NodeJS | ExpressJS | MongoDB",
+    url: "/images/girl-backend.jpg",
+    name: "Back-end",
+    desc: "I excel in backend development, leveraging Node.js, Express, and MongoDB to build powerful server-side solutions. This expertise enables me to create scalable APIs and handle data efficiently, complementing my frontend skills for comprehensive web development.",
+  },
+];
 const About = () => {
-  const cardData = [
-    {
-      id: 1,
-      skill: "React | Tailwind | Next",
-      url: "/images/girl-ui.jpg",
-      name: "Front-end",
-      desc: "I command HTML, CSS, and JavaScript proficiently, and my expertise extends to React, Tailwind CSS, and Next.js. This comprehensive skill set enables me to excel in frontend development, crafting dynamic and responsive user interfaces.",
-    },
-    {
-      id: 2,
-      skill: "NodeJS | ExpressJS | MongoDB",
-      url: "/images/girl-backend.jpg",
-      name: "Back-end",
-      desc: "I excel in backend development, leveraging Node.js, Express, and MongoDB to build powerful server-side solutions. This expertise enables me to create scalable APIs and handle data efficiently, complementing my frontend skills for comprehensive web development.",
-    },
-  ];
   return (
-    <div className=" h-full w-full px-6 flex flex-col items-center gap-10 md:px-32">
+    <div className=" h-full w-full px-6 flex flex-col items-center gap-4 md:px-32">
       {/* image heading block */}
-      <div className=" group transition-all ease-in-out duration-300 bg-primary/70 text-white flex flex-col justify-between items-center gap-10 rounded-lg px-6 py-4 hover:flex-row-reverse container shadow-lg md:px-10 md:flex-row">
+      <div className=" group transition-all ease-in-out duration-300 bg-primary/70 text-white flex flex-col justify-between items-center gap-10 rounded-lg px-6  container shadow-lg md:px-10 md:flex-row">
         <div className=" flex flex-col items-start gap-4">
           <h2 className=" main_heading">What Can I Do Best?</h2>
           <p className="text-left">
             I’m a Computer Science and Engineering graduate with strong web
             development and programming skills. Open to exciting opportunities
             and collaborations.
-            <br /> Let’s connect !
           </p>
         </div>
-        <Image
-          src={"/images/s6.svg"}
-          alt="image"
-          width={130}
-          height={130}
-          className=" mirror_flip"
-        />
+        <Image src={"/images/s6.svg"} alt="image" width={130} height={130} />
       </div>
       {/* achievement block */}
       <div className=" w-full flex flex-col md:flex-row md:justify-between">
--- components/utils/FlipCard.tsx
+++ components/utils/FlipCard.tsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
-const FlipCard = ({image,name,desc,skill}:any) => {
+const FlipCard = ({ image, name, desc, skill }: any) => {
   return (
     <div className="flip-card">
-    <div className="flip-card-inner">
-      <div className="flip-card-front">
-        <div className=' h-2/3 bg-white rounded-t-2xl relative pt-1'><Image className=' rounded-t-2xl absolute' src={image} alt='image' fill={true} /></div>
-        <div className=' h-1/3 rounded-b-2xl flex items-center flex-col pt-2 gap-1'>
-          <h3 className='text-xl font-semibold tracking-wide'>{name}</h3>
-          <p className='text-lg font-medium'>{skill}</p>
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          <div className=" bg-white rounded-t-2xl flex justify-center items-center">
+            <Image
+              className=" rounded-xl h-[18.5rem] w-auto shadow-lg shadow-blue-200"
+              src={image}
+              alt="image"
+              height={2000}
+              width={2000}
+            />
+          </div>
+          <br />
+          <div className=" h-1/3 rounded-b-2xl flex items-center flex-col pt-2 gap-2">
+            <h3 className="text-xl font-semibold tracking-wide">{name}</h3>
+            <p className="text-lg font-medium">{skill}</p>
+          </div>
+        </div>
+        <div className="flip-card-back shadow-md px-4 py-8 text-left">
+          <p>{desc}</p>
         </div>
       </div>
-      <div className="flip-card-back shadow-md px-4 py-8 text-left">
-        <p>{desc}</p>
-      </div>
     </div>
-  </div>
   );
-}
+};
 
-export default FlipCard
+export default FlipCard;
