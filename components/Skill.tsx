8ce58c7da1e73536476e716e837f18b15e5006cd
Pallavi <dpallavi854@gmail.com>
2023-08-27 20:12:07 +0530

    footer done

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -2,32 +2,21 @@ import Image from "next/image";
 import React from "react";
 
 const Skill = () => {
-  return <div className=" h-full w-full flex flex-col gap-10 justify-center items-center px-6">
-    <h2 className="main_heading text-primary">Skills and Experience</h2>
-    <div className="w-full h-[400px] rounded-lg border-primary border-4 bg-white/60 shadow-xl shelf-shadow">
-      <div className=" border-b-4 w-full h-[100px] border-primary flex justify-evenly items-end">
-        <Image src={"/images/java-icon.svg"} alt="react" width={50} height={50} />        
-        <Image src={"/images/mysql-icon.svg"} alt="react" width={50} height={50} />        
-        <Image src={"/images/nextjs-icon.svg"} alt="react" width={50} height={50} />        
+  return <div className=" h-full w-full px-6 flex justify-center items-center"> 
+      
+      <div className=" w-52 h-52 relative rounded-full bg-red-600">
+      <div className=" w-20 h-20 bg-pink-100 rounded-full absolute -top-24">I1</div>
+      <div className=" w-20 h-20 bg-pink-200 rounded-full absolute -top-0  -left-24">I2</div>
+      <div className=" w-20 h-20 bg-pink-300 rounded-full absolute bottom-0 -left-24">I3</div>
+      <div className=" w-20 h-20 bg-pink-400 rounded-full absolute -bottom-24 left-0">I4</div>
+      <div className=" w-20 h-20 bg-pink-500 rounded-full absolute -bottom-24 right-0">I5</div>
+      <div className=" w-20 h-20 bg-pink-600 rounded-full absolute -bottom-0 -right-24">I6</div>
+      <div className=" w-20 h-20 bg-pink-700 rounded-full absolute -top-0 -right-24">I7</div>
+      <div className=" w-20 h-20 bg-pink-800 rounded-full absolute -top-24 right-0">I8</div>
+      
+      
+      
       </div>
-      <div className=" border-b-4 w-full h-[100px] border-primary flex justify-evenly items-end">
-        <Image src={"/images/java-icon.svg"} alt="react" width={50} height={50} />        
-        <Image src={"/images/mysql-icon.svg"} alt="react" width={50} height={50} />        
-        <Image src={"/images/nextjs-icon.svg"} alt="react" width={50} height={50} />        
-      </div>
-      <div className=" border-b-4 w-full h-[100px] border-primary flex justify-evenly items-end">
-        <Image src={"/images/java-icon.svg"} alt="react" width={50} height={50} />        
-        <Image src={"/images/mysql-icon.svg"} alt="react" width={50} height={50} />        
-        <Image src={"/images/nextjs-icon.svg"} alt="react" width={50} height={50} />        
-      </div>
-      <div className=" w-full h-[100px] flex justify-evenly items-end">
-        <Image src={"/images/java-icon.svg"} alt="react" width={50} height={50} />        
-        <Image src={"/images/mysql-icon.svg"} alt="react" width={50} height={50} />        
-        <Image src={"/images/nextjs-icon.svg"} alt="react" width={50} height={50} />        
-      </div>
-     
-    </div>
-
   </div>;
 };
 
