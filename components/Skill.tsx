2d6c356a4ece26375ca3aa160a567ad75437b1be
Pallavi Dwivedi <121352105+pallaviDwivedi1605@users.noreply.github.com>
2024-07-18 17:03:00 +0000

    aaded resume

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -7,7 +7,7 @@ const Skill = () => {
   const SkillSection = () => {
     return (
       <div className=" flex justify-center items-center pb-6">
-        <div className=" w-52 h-52 relative rounded-full flex justify-center items-center bg-primary">
+        <div className=" w-52 h-52 relative rounded-full flex justify-center duration-150 items-center bg-primary">
           <Image
             src={"/images/s5.svg"}
             alt="image"
@@ -15,7 +15,7 @@ const Skill = () => {
             height={200}
             className="hover:scale-90"
           />
-          <div className=" w-20 h-20 bg-white rounded-full absolute -top-24 left-0 flex justify-center items-center group shadow-lg hover:shadow-xl">
+          <div className=" w-20 h-20 bg-white rounded-full absolute -top-24 left-0 duration-150 flex justify-center items-center group shadow-lg hover:shadow-xl">
             <Image
               src={"/images/java-icon.svg"}
               alt="java"
@@ -24,7 +24,7 @@ const Skill = () => {
               className=" group-hover:scale-125"
             />
           </div>
-          <div className=" w-20 h-20 bg-white rounded-full absolute -top-0  -left-24 flex justify-center items-center group shadow-lg hover:shadow-xl">
+          <div className=" w-20 h-20 bg-white rounded-full absolute -top-0  -left-24 flex duration-150 justify-center items-center group shadow-lg hover:shadow-xl">
             <Image
               src={"/images/react.svg"}
               alt="react js"
@@ -33,7 +33,7 @@ const Skill = () => {
               className=" group-hover:scale-125"
             />
           </div>
-          <div className=" w-20 h-20 bg-white rounded-full absolute bottom-0 -left-24 flex justify-center items-center group shadow-lg hover:shadow-xl">
+          <div className=" w-20 h-20 bg-white rounded-full absolute bottom-0 -left-24 flex duration-150 justify-center items-center group shadow-lg hover:shadow-xl">
             <Image
               src={"/images/nextjs-icon.svg"}
               alt="next js"
@@ -42,7 +42,7 @@ const Skill = () => {
               className=" group-hover:scale-125"
             />
           </div>
-          <div className=" w-20 h-20 bg-white rounded-full absolute -bottom-24 left-0 flex justify-center items-center group shadow-lg hover:shadow-xl">
+          <div className=" w-20 h-20 bg-white rounded-full absolute -bottom-24 left-0 flex duration-150 justify-center items-center group shadow-lg hover:shadow-xl">
             <Image
               src={"/images/mysql-icon.svg"}
               alt="mysql"
@@ -51,7 +51,7 @@ const Skill = () => {
               className=" group-hover:scale-125"
             />
           </div>
-          <div className=" w-20 h-20 bg-white rounded-full absolute -bottom-24 right-0 flex justify-center items-center group shadow-lg hover:shadow-xl">
+          <div className=" w-20 h-20 bg-white rounded-full absolute -bottom-24 right-0 flex duration-150 justify-center items-center group shadow-lg hover:shadow-xl">
             <Image
               src={"/images/nodejs-icon.svg"}
               alt="java"
@@ -60,7 +60,7 @@ const Skill = () => {
               className=" group-hover:scale-125"
             />
           </div>
-          <div className=" w-20 h-20 bg-white rounded-full absolute -bottom-0 -right-24 flex justify-center items-center group shadow-lg hover:shadow-xl">
+          <div className=" w-20 h-20 bg-white rounded-full absolute -bottom-0 -right-24 flex duration-150 justify-center items-center group shadow-lg hover:shadow-xl">
             <Image
               src={"/images/expressjs-icon.svg"}
               alt="java"
@@ -69,7 +69,7 @@ const Skill = () => {
               className=" group-hover:scale-125"
             />
           </div>
-          <div className=" w-20 h-20 bg-white rounded-full absolute -top-0 -right-24 flex justify-center items-center group shadow-lg hover:shadow-xl">
+          <div className=" w-20 h-20 bg-white rounded-full absolute -top-0 -right-24 flex duration-150 justify-center items-center group shadow-lg hover:shadow-xl">
             <Image
               src={"/images/tailwindcss.svg"}
               alt="java"
@@ -78,7 +78,7 @@ const Skill = () => {
               className=" group-hover:scale-125"
             />
           </div>
-          <div className=" w-20 h-20 bg-white rounded-full absolute -top-24 right-0 flex justify-center items-center group shadow-lg hover:shadow-xl">
+          <div className=" w-20 h-20 bg-white rounded-full absolute -top-24 right-0 flex duration-150 justify-center items-center group shadow-lg hover:shadow-xl">
             <Image
               src={"/images/mongodb-icon.svg"}
               alt="java"
@@ -95,7 +95,7 @@ const Skill = () => {
   const Experince = () => {
     return (
       <div className="flex flex-col gap-16 h-fit">
-        <div className="max-w-[500px] min-w-max h-32 md:h-24 px-4 bg-white shadow-xl shadow-rose-400/50 flex gap-5 justify-center flex-col md:flex-row md:justify-between items-center rounded-xl">
+        <div className="max-w-[500px] hover:shadow=-md duration-200 min-w-max h-32 md:h-24 px-4 bg-white shadow-xl duration-150 shadow-rose-400/50 flex gap-5 justify-center flex-col md:flex-row md:justify-between items-center rounded-xl">
           <p className="text-primary font-semibold text-xl">
             Sept’23 - Present
           </p>
@@ -104,7 +104,7 @@ const Skill = () => {
             <p className=" text-base font-normal">Continuum Global</p>
           </div>
         </div>
-        <div className="max-w-[500px] min-w-max h-32 md:h-24 px-4 bg-white shadow-xl shadow-rose-400/50 flex gap-5 justify-center flex-col md:flex-row md:justify-between items-center rounded-xl">
+        <div className="max-w-[500px] min-w-max h-32 md:h-24 px-4 bg-white shadow-xl duration-150 shadow-rose-400/50 flex gap-5 justify-center flex-col md:flex-row md:justify-between items-center rounded-xl">
           <p className="text-primary font-semibold text-xl">Feb’23 - May’23</p>
           <div className=" flex flex-col items-center justify-center">
             <h4 className=" text-2xl font-medium">React Developer Intern</h4>
@@ -114,7 +114,7 @@ const Skill = () => {
           </div>
         </div>
 
-        <div className=" px-10 py-2 gap-5 flex flex-col rounded-xl justify-center items-center">
+        <div className=" px-10 py-2 gap-5 flex flex-col rounded-xl justify-center duration-150 items-center">
           <h3 className="text-2xl font-semibold text-primary border-b-4 border-primary/70 rounded-md w-fit">
             Interested In:
           </h3>
@@ -126,9 +126,9 @@ const Skill = () => {
             </div>
             <Link
               href={"mailto:dpallavi854@gmail.com"}
-              className=" group flex items-center w-fit h-fit bg-white shadow-md shadow-red-200 text-primary hover:text-white md:rounded-full rounded-xl px-4 py-1 tracking-wide font-medium text-lg hover:bg-primary hover:shadow-xl duration-500"
+              className=" group flex items-center w-fit h-fit bg-white shadow-md duration-150 shadow-red-200 text-primary hover:text-white md:rounded-full rounded-xl px-4 py-1 tracking-wide font-medium text-lg hover:bg-primary hover:shadow-xl duration-500"
             >
-              <IoMdCall className="bg-primary text-[20px] h-8 w-8 rounded-full p-2 mr-1 text-white group-hover:text-primary group-hover:bg-white duration-300 " />
+              <IoMdCall className="bg-primary text-[20px] h-8 w-8 rounded-full p-2 mr-1 text-white duration-150 group-hover:text-primary group-hover:bg-white duration-300 " />
               Hire Me
             </Link>
           </div>
